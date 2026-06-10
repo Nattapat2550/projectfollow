21a577f250e4605d3827430f063218d7307cc660
Cookie4686 <cookieandcake1105@gmail.com>
2026-06-11 02:21:34 +0700

    add immigrant mockdata

--- frontend/src/app/immigrant/page.tsx
+++ frontend/src/app/immigrant/page.tsx
@@ -1,3 +1,11 @@
-export default function Immigrant() {
-	return <h1>แอบเข้า</h1>;
+import { getImmigrantData } from "@/lib/server/immigrant";
+
+export default async function Immigrant() {
+	const data = await getImmigrantData();
+	return (
+		<div>
+			<h1>แอบเข้า</h1>
+			<pre>{JSON.stringify(data, undefined, " ")}</pre>;
+		</div>
+	);
 }
